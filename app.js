49c7cb362c80994d43264f875b047995965d487e
Rob Conner <rjconner@mac.com>
2017-07-10 21:02:30 -0700

    initial refactor

--- app.js
+++ app.js
@@ -6,40 +6,48 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function() {
+  let wordToGuess = selectWord(words);
+  let wrongAttempts = 0;
+
   let guessBtn = document.getElementById('guess');
   let guessBox = document.querySelector('.guess-box');
 
-  let wordToGuess = '';
-  let wrongAttempts = 0;
-
   // adds event listeners to the guess text box and button
-  guessBtn.addEventListener('click', checkLetter);
+  guessBtn.addEventListener('click', main);
 
   guessBox.focus();
   guessBox.addEventListener('keyup', keyUpHandler(event));
 
+  // handler for keyup event
   function keyUpHandler(event) {
-    if (event.keyCode === 13) checkLetter();
+    if (event.keyCode === 13) main();
   }
 
-  wordToGuess = selectWord(words);
+  // controls flow of game
+  function main() {
+    let letter = getLetter();
+    updateGameStatus(letter);
+    checkEndOfGame();
+  }
 
-  // gets, validates, and checks for guessed letter
-  function checkLetter() {
+  // gets and validates guessed letter
+  function getLetter() {
     let guess = '';
     let letter = document.querySelector('.guess-box').value;
 
+    // clears guess field
     document.querySelector('.guess-box').value = '';
 
     if (letter.length) {
       if ((letter.charCodeAt(0) > 96 && letter.charCodeAt(0) < 123) ||
         (letter.charCodeAt(0) > 64 && letter.charCodeAt(0) < 91)) {
-          updateAttempt(letter.toLowerCase());
+          return letter.toLowerCase();
       }
     }
   }
 
-  function updateAttempt(letter) {
+  // updates game status for guess attempt
+  function updateGameStatus(letter) {
     if (wordToGuess.indexOf(letter) === -1) {
       wrongAttempts++;
 
@@ -53,36 +61,37 @@ document.addEventListener('DOMContentLoaded', function() {
       let trackProgress = document.getElementsByTagName('h1')[0].innerText.split('');
 
       for (var i = 0; i < wordToGuess.length; i++) {
-        if (wordToGuess[i] === letter) {
-          trackProgress[i] = letter;
-        }
+        if (wordToGuess[i] === letter) trackProgress[i] = letter;
       }
 
       document.getElementsByTagName('h1')[0].innerText = trackProgress.join('');
-
-    }
-
-    if (wrongAttempts === 7) {
-      document.getElementById('result').classList.add('lost');
-      document.getElementsByTagName('p')[0].innerText = 'Game Over! The word was: ' + wordToGuess + '.';
-
-      guessBtn.removeEventListener('click', checkLetter);
-      guessBox.removeEventListener('keyup', keyUpHandler);
-    }
-
-    if (wordToGuess === document.getElementsByTagName('h1')[0].innerText) {
-      document.getElementById('result').classList.add('won');
-      document.getElementsByTagName('p')[0].innerText = 'Congrats! You figured it out!';
-
-      guessBtn.removeEventListener('click', checkLetter);
-      guessBox.removeEventListener('keyup', keyUpHandler);
     }
   }
 
+  // checks for the end of the game, reports result, and removes event listeners
+  // from guess field
+  function checkEndOfGame() {
+    if (wrongAttempts === 7 || wordToGuess === document.getElementsByTagName('h1')[0].innerText) {
+      let resultEle = document.getElementById('result');
+      let messageEle = document.getElementsByTagName('p')[0];
+
+      if (wrongAttempts === 7) {
+        resultEle.classList.add('lost');
+        messageEle.innerText = 'Game Over! The word was: ' + wordToGuess + '.';
+      }
+
+      if (wordToGuess === document.getElementsByTagName('h1')[0].innerText) {
+        resultEle.classList.add('won');
+        messageEle.innerText = 'Congrats! You figured it out!';
+      }
+
+      guessBtn.removeEventListener('click', main);
+      guessBox.removeEventListener('keyup', keyUpHandler);
+    }
+  }
 });
 
-
-// function to select random word and display placeholder
+// function to select random word and display placeholder based on length of word
 function selectWord(array) {
   let index = Math.floor(Math.random() * (words.length - 1));
   let word = words[index];
@@ -93,7 +102,7 @@ function selectWord(array) {
   }
 
   let h1Ele = document.getElementsByTagName('h1')[0];
-  console.log(word);
   h1Ele.innerText = progress;
+
   return word;
 }
